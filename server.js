d53c4fc400d038bbf215e528fdd1199b000b51ce
shashanksoni61 <shashanksoni61@gmail.com>
2021-08-07 00:42:48 +0530

    changed node environment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // connecting to the database
@@ -24,14 +24,23 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => res.send('API running'));
-
 // defining routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/post'));
 
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+//set port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on PORT ${PORT}`));
